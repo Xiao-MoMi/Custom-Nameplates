055e4e835cbf97a13013a5bed8b9e3ef76003d7d
XiaoMoMi <972454774@qq.com>
2024-01-20 05:17:29 +0800

    zip

--- api/src/main/java/net/momirealms/customnameplates/api/manager/WidthManager.java
+++ api/src/main/java/net/momirealms/customnameplates/api/manager/WidthManager.java
@@ -1,5 +1,16 @@
 package net.momirealms.customnameplates.api.manager;
 
+import net.momirealms.customnameplates.api.common.Key;
+import net.momirealms.customnameplates.api.mechanic.font.FontData;
+import org.jetbrains.annotations.Nullable;
+
 public interface WidthManager {
 
+    boolean registerFontData(Key key, FontData fontData);
+
+    boolean unregisterFontData(Key key);
+
+    @Nullable FontData getFontData(Key key);
+
+    int getTextWidth(String textWithTags);
 }
