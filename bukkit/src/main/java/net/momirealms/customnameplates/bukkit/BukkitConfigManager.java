1d4fac8cf5a6a09c2d4b72eb77c9d31ebc6f8a05
XiaoMoMi <70987828+Xiao-MoMi@users.noreply.github.com>
2024-09-26 01:05:13 +0800

    checkpoint

--- bukkit/src/main/java/net/momirealms/customnameplates/bukkit/BukkitConfigManager.java
+++ bukkit/src/main/java/net/momirealms/customnameplates/bukkit/BukkitConfigManager.java
@@ -1,8 +1,13 @@
 package net.momirealms.customnameplates.bukkit;
 
 import net.momirealms.customnameplates.api.ConfigManager;
+import net.momirealms.customnameplates.api.CustomNameplates;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class BukkitConfigManager extends ConfigManager {
 
@@ -13,10 +18,55 @@ public class BukkitConfigManager extends ConfigManager {
         this.plugin = plugin;
     }
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Override
-    public void saveResource(String filePath) {
-        if (!new File(plugin.getDataDirectory().toFile(), filePath).exists()) {
-            plugin.getBootstrap().saveResource(filePath, false);
+    public void saveResource(@NotNull String resourcePath) {
+        if (resourcePath.isEmpty()) {
+            throw new IllegalArgumentException("ResourcePath cannot be null or empty");
+        }
+
+        resourcePath = resourcePath.replace('\\', '/');
+        InputStream in = getResource(resourcePath);
+        if (in == null) {
+            return;
+        }
+
+        File outFile = new File(BukkitCustomNameplates.getInstance().getBootstrap().getDataFolder(), resourcePath);
+        int lastIndex = resourcePath.lastIndexOf('/');
+        File outDir = new File(BukkitCustomNameplates.getInstance().getBootstrap().getDataFolder(), resourcePath.substring(0, Math.max(lastIndex, 0)));
+
+        if (!outDir.exists()) {
+            outDir.mkdirs();
+        }
+
+        try {
+            if (!outFile.exists()) {
+                OutputStream out = new FileOutputStream(outFile);
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                out.close();
+                in.close();
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Nullable
+    public static InputStream getResource(@NotNull String filename) {
+        try {
+            URL url = BukkitCustomNameplates.getInstance().getBootstrap().getClass().getClassLoader().getResource(filename);
+            if (url == null) {
+                return null;
+            }
+            URLConnection connection = url.openConnection();
+            connection.setUseCaches(false);
+            return connection.getInputStream();
+        } catch (IOException ex) {
+            return null;
         }
     }
 }
